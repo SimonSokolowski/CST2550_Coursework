46324d1b5415f28e8068139d2e1598e6014b0d87
Simon <simon.sokolowski@outlook.com>
2023-12-17 11:55:35 +0000

    header for person

---
+++ Person.h
@@ -0,0 +1,23 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+
+class Person {
+private:
+    std::string name;
+    std::string address;
+    std::string email;
+
+public:
+    std::string getName() const;
+    void setName(const std::string& name);
+
+    std::string getAddress() const;
+    void setAddress(const std::string& address);
+
+    std::string getEmail() const;
+    void setEmail(const std::string& email);
+};
+
+#endif // PERSON_H
