ebc461b3afee597adbeebf914bd95f02abfc82dd
Simon <simon.sokolowski@outlook.com>
2023-12-17 15:01:29 +0000

    add header for member

---
+++ include/Member.h
@@ -0,0 +1,23 @@
+#ifndef MEMBER_H
+#define MEMBER_H
+
+#include "Person.h"
+#include "Book.h"
+#include <vector>
+#include <string>
+
+class Member : public Person {
+private:
+    int memberID;
+    std::vector<Book*> booksLoaned;
+
+public:
+    Member(int memberID, const std::string &name, const std::string &address, const std::string &email);
+
+    std::string getMemberID();
+    
+    std::vector<Book*> getBooksBorrowed() const;
+    void setBooksBorrowed(Book *book);
+};
+
+#endif
