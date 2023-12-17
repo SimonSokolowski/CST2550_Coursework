0b593d8794645011b851362e80aacbe9b3f7a1d6
Simon <simon.sokolowski@outlook.com>
2023-12-17 12:12:53 +0000

    add book types header

---
+++ include/book.h
@@ -0,0 +1,26 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <string>
+
+enum class BookType {
+    ScienceFiction,
+    Satire,
+    Drama,
+    ActionAndAdventure,
+    Romance,
+    Mystery,
+    Horror,
+    Health,
+    Guide,
+    Diaries,
+    Comics,
+    Journals,
+    Biographies,
+    Fantasy,
+    History,
+    Science,
+    Art
+};
+
+#endif
