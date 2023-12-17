c3e5f203842f2594aa32dfb4097d5f871dee5154
Simon <simon.sokolowski@outlook.com>
2023-12-17 13:07:11 +0000

    add header for librarian

---
+++ include/Librarian.h
@@ -0,0 +1,27 @@
+#ifndef LIBRARIAN_H
+#define LIBRARIAN_H
+
+#include "Person.h"
+#include "Book.h"
+
+class Librarian : public Person {
+private:
+    int staffID;
+    int salary;
+
+public:
+    Librarian(int staffID, const std::string &name, const std::string &address, const std::string &email, int salary);
+
+    void addMember(int staffID, const std::string &name, const std::string &address, const std::string &email, int salary);
+    void issueBook(int memberID, int bookID);
+    void returnBook(int memberID, int bookID);
+    void displayBorrowedBooks(int memberID);
+    void calcFine(int memberID);
+    
+    int getStaffID();
+    int setStaffID();
+    int getSalary();
+    void setSalary(int salary);
+};
+
+#endif
