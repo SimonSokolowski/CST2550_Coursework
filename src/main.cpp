7fbe772819d4d316ed436724ac6b9a2413e40d57
Simon <simon.sokolowski@outlook.com>
2023-12-28 12:05:54 +0000

    add ability to read from file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include "Librarian.h"
 #include "Member.h"
+#include "Book.h"
 
-Librarian getLibrarianDetails() {
+std::vector<Book> globalBooks; // Global vector to store books
+
+Librarian getLibrarianDetails()
+{
     int staffID;
     std::string name, address, email;
     int salary;
@@ -29,18 +34,50 @@ Librarian getLibrarianDetails() {
     return Librarian(staffID, name, address, email, salary);
 }
 
-int main() {
+void populateBooks()
+{
+
+    // REFERENCE: https://cplusplus.com/doc/tutorial/files/
+    // I learnt from and used the code in the above link to read from
+    // a file input by the user.
+
+    std::string line;
+    std::string fileName;
+
+    std::cout << "Enter book collection filename: ";
+    std::cin >> fileName;
+
+    // Construct the relative path to the books directory
+    std::string relativePath = "../books/" + fileName;
+
+    std::ifstream myfile(relativePath);
+
+    if (myfile.is_open())
+    {
+        while (getline(myfile, line))
+        {
+            std::cout << line << '\n';
+        }
+        myfile.close();
+    }
+
+    else {
+        std::cout << "Unable to open file\n";
+    }
+}
+
+int main()
+{
+    populateBooks();
 
     std::cout << "Welcome to the Library System\n";
-    
+
     Librarian librarian = getLibrarianDetails();
 
     librarian.addMember();
 
     // Display the librarian's staff ID as a confirmation
-    //std::cout << "Librarian's Staff ID: " << librarian.getStaffID() << "\n";
-
-
+    // std::cout << "Librarian's Staff ID: " << librarian.getStaffID() << "\n";
 
     return 0;
 }
