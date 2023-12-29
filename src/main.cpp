e758568f74fb22ee4e1257a86b49024d4ac5f3d9
Simon <simon.sokolowski@outlook.com>
2023-12-29 15:58:06 +0000

    add csv file reading and upload to vector and test output to see if work

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "Librarian.h"
 #include "Member.h"
 #include "Book.h"
@@ -34,6 +36,26 @@ Librarian getLibrarianDetails()
     return Librarian(staffID, name, address, email, salary);
 }
 
+// Check for quotes and temporarily replaces commas with another character
+std::stringstream processLine(const std::string &line)
+{
+    std::stringstream linestream;
+    bool inQuotes = false;
+    for (char c : line)
+    {
+        if (c == '\"')
+        {
+            inQuotes = !inQuotes;
+        }
+        else if (c == ',' && inQuotes)
+        {
+            c = '|'; // Temporarily replace commas with another character
+        }
+        linestream << c;
+    }
+    return linestream;
+}
+
 void populateBooks()
 {
 
@@ -44,7 +66,7 @@ void populateBooks()
     std::string line;
     std::string fileName;
 
-    std::cout << "Enter book collection filename: ";
+    std::cout << "Enter book collection filename:" << std::endl;
     std::cin >> fileName;
 
     // Construct the relative path to the books directory
@@ -58,16 +80,49 @@ void populateBooks()
         while (getline(myfile, line))
         {
             if (isFirstLine)
-            { // Skip the first line
+            { // Skip the first line (column headers)
                 isFirstLine = false;
                 continue;
             }
 
-            std::cout << line << '\n';
+            // Book titles sometimes contain commas, which confuse the code (comma separated values are used)
+
+            std::stringstream linestream = processLine(line);
+
+            int bookID, pageCount;
+            std::string bookName, authorFirstName, authorLastName, bookType;
+            std::string value;
+
+            getline(linestream, value, ',');
+            bookID = std::stoi(value);
+            std::cout << "Book ID: " << bookID << std::endl;
+
+            getline(linestream, bookName, ',');
+
+            // Replace the temporary character back to a comma if it's in the title
+            std::replace(bookName.begin(), bookName.end(), '|', ',');
+
+            getline(linestream, value, ',');
+            pageCount = std::stoi(value);
+            getline(linestream, authorFirstName, ',');
+            getline(linestream, authorLastName, ',');
+            getline(linestream, bookType);
+
+            // REFERENCE: Above code for reading csv file and pushing to vector is
+            // loosely based on fiji885's comment on Apr 14, 2019
+            // https://cplusplus.com/forum/beginner/252267/
+
+            // Remove trailing newline or other characters
+            bookType.erase(std::remove(bookType.begin(), bookType.end(), '\n'), bookType.end());
+            bookType.erase(std::remove(bookType.begin(), bookType.end(), '\r'), bookType.end());
+
+            // Create a new Book object with the values and add it to the vector
+            Book tempBook(bookID, bookName, authorFirstName, authorLastName);
+
+            globalBooks.push_back(tempBook);
         }
         myfile.close();
     }
-
     else
     {
         std::cout << "Unable to open file\n";
@@ -77,6 +132,11 @@ void populateBooks()
 int main()
 {
     populateBooks();
+
+    for (const Book &book : globalBooks)
+    {
+        std::cout << "Book ID: " << book.getBookID() << "\n";
+    }
 
     std::cout << "\n";
     std::cout << "Welcome to the Library System\n";
